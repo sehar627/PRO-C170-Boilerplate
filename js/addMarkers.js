07918d6d4c640895ea9c130212abf7d454f92c0f
sehar627 <62961629+sehar627@users.noreply.github.com>
2024-03-16 17:28:33 +0530

    Update addMarkers.js

--- js/addMarkers.js
+++ js/addMarkers.js
@@ -1,5 +1,22 @@
 AFRAME.registerComponent("create-markers", {
+   init: async function() {
+
+    var mainScene = document.querySelector("#main-scene");
   
   //Add Code Here!
+  dishes.map(dish => {
+      var marker = document.createElement("a-marker");   
+      marker.setAttribute("id", dish.id);
+      marker.setAttribute("type", "pattern");
+      marker.setAttribute("url", dish.marker_pattern_url);
+      marker.setAttribute("cursor", {
+        rayOrigin: "mouse"
+      });
+
+      //set the markerhandler component
+      marker.setAttribute("markerhandler", {});
+      mainScene.appendChild(marker);
+
   
   });
+   })
